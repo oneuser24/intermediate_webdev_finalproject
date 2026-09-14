6c72886fb9ce7ac0a4dcdcb70d620a2b363d213d
oneuser24 <bohdan.senyuk@colorado.edu>
2026-09-13 19:25:43 -0600

    Added two functions

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const calculate = () => {
   let p = Number(document.getElementById("principal").value);
   let r = Number(document.getElementById("rate").value);
   let t = Number(document.getElementById("time").value);
-  let simpleInterest = (p * r * t) / 100;
-  let amount = p - simpleInterest;
+  let simpleInterest = calculateSimpleInterest(p, r, t);
+  let amount = calculateTotalPayableAmount(p, simpleInterest);
 
   let result = document.getElementById("result");
 
@@ -11,3 +11,14 @@ const calculate = () => {
   <div>Total Interest: <span>${simpleInterest.toFixed(2)}</span></div>
   <div>Total Amount: <span>${amount.toFixed(2)}</span></div>`;
 };
+
+const calculateSimpleInterest = (principal, rate, time) => {
+  return (principal * rate * time) / 100;
+}
+
+const calculateTotalPayableAmount = (principal, interestAmount) => {
+  return principal - interestAmount;
+}
+
+if (typeof module !== 'undefined')
+  module.exports = { calculateSimpleInterest, calculateTotalPayableAmount, calculate };
